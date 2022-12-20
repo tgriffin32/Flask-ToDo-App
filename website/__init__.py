db1ce78beb18e679d3522ca6aa84d1e172b30ba7
= <toby.n.griffin-1@ou.edu>
2022-12-20 03:30:59 -0600

    created database

--- website/__init__.py
+++ website/__init__.py
@@ -1,8 +1,16 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy 
+from os import path 
+
+# creates database object 
+db = SQLAlchemy()
+DB_NAME = "database.db "
 
 def create_app():
     app = Flask(__name__)   # intitializing flask app
     app.config['SECRET_KEY'] = 'jfkadfjsdof'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}' # means our db is store at this location
+    db.init_app(app)
 
     # need to register blueprints from views and auth
     from .views import views
@@ -11,4 +19,14 @@ def create_app():
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
+    from .models import User, Note   
+
+    create_database(app)
+
     return app 
+
+def create_database(app):
+    if not path.exists('website/' + DB_NAME):
+        with app.app_context():
+            db.create_all()
+        print('Created Database!')
---
+++ website/models.py
@@ -0,0 +1,21 @@
+from . import db 
+from flask_login import UserMixin 
+from sqlalchemy.sql import func 
+
+''' db model is a layout for how a db object will be stored in a databse '''
+
+class Note(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    data = db.Column(db.String(10000))
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
+
+    # all notes must be associated with a user, uses a foreign key that references a key form another model
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)    # primary key
+    email = db.Column(db.String(150), unique=True)
+    password = db.Column(db.String(150))
+    first_name = db.Column(db.String(150))
+    last_name = db.Column(db.String(150))
+    notes = db.relationship('Note')
