f581773a31c702b95a8236a3bbfacabfb6b93147
= <toby.n.griffin-1@ou.edu>
2022-12-20 23:08:44 -0600

    created new user accounts

--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,8 @@
-from flask import Blueprint, render_template, request, flash 
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import User 
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db 
+
 
 auth = Blueprint('auth', __name__)
 
@@ -21,24 +25,27 @@ def logout():
 def sign_up():
     if request.method == 'POST':
         email = request.form.get('email')
-        firstName = request.form.get('firstName')
-        lastName = request.form.get('lastName')
+        first_name = request.form.get('firstName')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
 
         if len(email) < 4:
             flash('Email must be greater than 4 charcters.', category='error')
-        elif len(firstName) < 2:
+        elif len(first_name) < 2:
             flash('First name must be greater than 1 character', category='error')
-        elif len(lastName) < 2:
-            flash('Last name must be greater than 1 character.', category='error')
         elif password1 != password2:
             flash('Passwords don\'t match.', category='error')
         elif len(password1) < 7:
             flash('Password must be at least 7 characters.')
         else:
             # add user to database
+            new_user = User(email=email, first_name=first_name, password=generate_password_hash(password1, method='sha256'))
+            db.session.add(new_user)
+            db.session.commit()
+
             flash('Account created!', category='success')
+            return redirect(url_for('views.home'))
+
 
 
     return render_template("sign_up.html")
