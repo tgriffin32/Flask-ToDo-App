0335ed6f2941c6016c4defce89c0ae76462f8c8b
= <toby.n.griffin-1@ou.edu>
2022-12-20 23:45:54 -0600

    logged user in and check login

--- website/views.py
+++ website/views.py
@@ -5,11 +5,13 @@ where users can visit
 """
 
 from flask import Blueprint, render_template
+from flask_login import login_required, current_user 
 
 views = Blueprint('views', __name__) #defines a views blueprint
 
 @views.route('/')   # when this root is hit, def home will execute
+@login_required
 def home():
-    return render_template("home.html")
+    return render_template("home.html", user=current_user)
 
 
